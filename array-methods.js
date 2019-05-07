370c47daa89735f7389dd7bc3f28dbbee03ae24e
Bruce <bruce@yogainternational.com>
2019-05-06 22:05:27 -0400

    updates tests

---
+++ array-methods.js
@@ -0,0 +1,16 @@
+/*
+1. Write a function called arrayCopier that that takes in an array and any validJS
+datatype (ie. String, Number, Array, etc) and returns and array with the new item added to the end.
+
+This function should return a COPY of the original array and NOT the original itself!
+
+To get the most out of this exercise, try to use 2 or more array methods in your implementation.
+*/
+
+// Example
+/***
+ *
+ * var arr1 = ['1', '2', 3, ['innerArrayItem', [2, 3]], 'Last Item'];
+ * arrayCopier(arr1, 'newLastItem') -> would return ['1', '2', 3, ['innerArrayItem', [2, 3]], 'Last Item', 'newLastItem']
+ *
+ */
---
+++ array-methods.spec.js
@@ -0,0 +1,20 @@
+describe('arrayCopier', () => {
+  const originalArray = ['1', '2', '3', '4', 'cow', '6'];
+  it('should return an array', () => {
+    expect(Array.isArray(arrayCopier(originalArray, '7'))).to.equal(true);
+  });
+  it('should not return the original array', () => {
+    expect(arrayCopier(originalArray, '7')).to.not.equal(originalArray);
+  });
+  it('should return a copy of the original array with the new item added to the end', () => {
+    expect(arrayCopier(originalArray, '7')).to.deep.equal([
+      '1',
+      '2',
+      '3',
+      '4',
+      'cow',
+      '6',
+      '7',
+    ]);
+  });
+});
